36d2f0aa9f2a8af3a79535c0c8209b42c059444f
Yair Avalos <avalos.consulting.gdl@gmail.com>
2021-10-22 18:32:26 -0500

    feat: serializer ProductVendor has been added

--- ShoppingCartAPI/App_Shop_Mgmt/serializers.py
+++ ShoppingCartAPI/App_Shop_Mgmt/serializers.py
@@ -1,14 +1,26 @@
 # Standard Libraries and Packages
+from django.db import models
 from rest_framework import serializers
 
 # Models
-from .models import ProductCatalog, ProductLogistics, ProductStock
+from .models import ProductCatalog, ProductLogistics, ProductStock, ProductVendor
 
 # Create Serializers
+class ProductVendorSerializer(serializers.ModelSerializer):
+    """
+    This serializer helps as interface for Product Vendor Model
+    """
+
+    class Meta:
+        model = ProductVendor
+        fields = ['id','vendor_company_name']
+
+
 class ProductCatalogSerializer(serializers.ModelSerializer):
     """
     This serializer helps as interface for Product Catalog Model
     """
+    product_vendor = ProductVendorSerializer()
 
     class Meta:
         model = ProductCatalog
@@ -19,11 +31,11 @@ class ProductStockSerializer(serializers.ModelSerializer):
     """
     This serializer helps as interface for Product Stock Model
     """
+    product = ProductCatalogSerializer()
 
     class Meta:
         model = ProductStock
         fields = ['id','product','product_stock_qty','product_stock_status_date']
-        depth = 1
 
 
 class ProductLogisticSerializer(serializers.ModelSerializer):
