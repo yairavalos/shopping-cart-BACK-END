9c41b608f9aebe8add9e5019e6d3f1f840c1708e
Yair Avalos <avalos.consulting.gdl@gmail.com>
2021-10-22 19:01:15 -0500

    feat: End-Points /purchase_order and /shopping_cart ListCreate has been enabled for POST

--- ShoppingCartAPI/App_User_Mgmt/urls.py
+++ ShoppingCartAPI/App_User_Mgmt/urls.py
@@ -4,13 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 # Views
-from .views import UsersIntroAPI, UserJobOrderList, UserShoppingCartList
+from .views import UsersIntroAPI, UserJobOrderListCreate, UserShoppingCartListCreate
 
 app_name = 'App_User_Mgmt'
 
 # End-Points
 urlpatterns = [
     path('', UsersIntroAPI.as_view()),
-    path('purchase_order/', UserJobOrderList().as_view()),
-    path('shopping_cart/', UserShoppingCartList().as_view()),
+    path('purchase_order/', UserJobOrderListCreate().as_view()),
+    path('shopping_cart/', UserShoppingCartListCreate().as_view()),
 ]
